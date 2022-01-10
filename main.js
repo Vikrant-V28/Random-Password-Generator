8d001f714edc2d9b70af1daa6c2bf96f54dbb708
Vikrant <85709371+Vikrant-V28@users.noreply.github.com>
2022-01-10 15:19:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-<script type="text/javascript">
-      function getPassword() {
-        var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$&_";
-        var passwordLength = 10;
-        var password = "";
-
-        for (var i = 0; i < passwordLength; i++) {
-          var randomNumber = Math.floor(Math.random() * chars.length);
-          password += chars.substring(randomNumber, randomNumber + 1);
-        }
-        document.getElementById("password").value = password;
+function getPassword() {
+      var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$&_";
+      var passwordLength = 10;
+      var password = "";
+      
+      for (var i = 0; i < passwordLength; i++) {
+            var randomNumber = Math.floor(Math.random() * chars.length);
+            password += chars.substring(randomNumber, randomNumber + 1);
       }
-</script>
+      
+      document.getElementById("password").value = password;
+}
